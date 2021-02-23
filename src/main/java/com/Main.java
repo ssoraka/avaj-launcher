99bc8daf1a3852331db764a381be5c347d3a8c1f
root <ssoraka@student.21-school.ru>
2021-02-23 21:22:14 +0300

    create script for program building

--- src/main/java/com/Main.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public class Main {
-    public static void main(String[] args) {
-        final Test test = new Test(1);
-        System.out.println("hello!");
-    }
-}
--- src/main/java/com/Test.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Test {
-    int i;
-
-    public Test(int i) {
-        this.i = i;
-    }
-}
